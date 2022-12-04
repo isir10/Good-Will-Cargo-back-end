f8e9a3bd13756242dbebf179e9141f36d706fe2a
James Mburu Githire <jamesmburug24@gmail.com>
2022-12-04 19:28:07 +0300

    GWC-8 added login, logout and /me routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :cargo_ships, only:[:create, :index, :update, :destroy]
+  resources :user_cargos, only:[:index, :show]
+  resources :users
+  post "/signup", to: "users#create"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  get "/me", to: "users#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
