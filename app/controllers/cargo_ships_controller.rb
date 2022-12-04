6f35d258fe22101be7a6258bf970aa33409ec510
James Mburu Githire <jamesmburug24@gmail.com>
2022-12-04 19:30:26 +0300

    GWC-8 created index and show method for CargoShips

--- app/controllers/cargo_ships_controller.rb
+++ app/controllers/cargo_ships_controller.rb
@@ -1,2 +1,8 @@
 class CargoShipsController < ApplicationController
+    def index
+        render json: CargoShips.all
+    end
+    def show
+        render json: CargoShips.find(params[:id])
+    end
 end
