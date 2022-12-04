e57a23c853e40c3b1f2cda739382c2320af278d6
James Mburu Githire <jamesmburug24@gmail.com>
2022-12-04 19:35:23 +0300

    GWC-8 added er-handling for unprocessable entities

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity_method
+    private
+    def unprocessable_entity_method e
+        render json: {errors: e.record.errors.full_messages}, status: :unprocessable_entity
+    end
 end
