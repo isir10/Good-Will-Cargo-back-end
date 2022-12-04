77655b0e774f62cd79a5077eb431b0641838c486
James Mburu Githire <jamesmburug24@gmail.com>
2022-12-04 19:34:07 +0300

    GWC-8 added error handling record_not_found

--- app/controllers/cargo_ships_controller.rb
+++ app/controllers/cargo_ships_controller.rb
@@ -1,8 +1,13 @@
 class CargoShipsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_method
     def index
         render json: CargoShips.all
     end
     def show
         render json: CargoShips.find(params[:id])
+    end
+    private
+    def record_not_found_method
+        render json: { error: "Cargo-ship Not Found" }, status: :not_found
     end
 end
