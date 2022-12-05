e61c564056d373650edc9d6efae616d85830e9c5
James Mburu Githire <jamesmburug24@gmail.com>
2022-12-05 16:12:23 +0300

    GWC-8 fixed attributes

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :username, :password_digest, :email, :bio, :img_url
+  attributes :id, :username, :password_digest, :email, :bio, :img_url
 end
