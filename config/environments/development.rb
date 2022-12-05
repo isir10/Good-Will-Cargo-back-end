52ed77e73b2255ccd38203d77d4ece89bcaf4100
James Mburu Githire <jamesmburug24@gmail.com>
2022-12-05 16:14:37 +0300

    GWC-8 added hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.hosts << "good-will-cargo-production.up.railway.app"
+  config.hosts << "good-will-cargo-spark-production.up.railway.app"
   
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.hosts << "good-will-cargo-production.up.railway.app"
+  config.hosts << "good-will-cargo-spark-production.up.railway.app"
 
   # Code is not reloaded between requests.
   config.cache_classes = true
