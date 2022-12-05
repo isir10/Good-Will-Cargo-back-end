b2631b38dce087bd388c42453529af981cabfd78
James Mburu Githire <jamesmburug24@gmail.com>
2022-12-05 07:28:29 +0300

    GWC-8 added update action

--- app/controllers/cargo_ships_controller.rb
+++ app/controllers/cargo_ships_controller.rb
@@ -6,8 +6,17 @@ rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_method
     def show
         render json: CargoShip.find(params[:id])
     end
+    def update
+        cargo_ship = CargoShip.find(params[:id])
+        cargo_ship.update!(cargo_ship_params)
+        render json: cargo_ship, status: :accepted
+    end
     private
     def record_not_found_method
         render json: { error: "Cargo-ship Not Found" }, status: :not_found
     end
+    def cargo_ship_params
+        params.permit(:remaining)
+    end
+
 end
