7b936ba8805961f16059d4fd5649f42faa1088f6
James Mburu Githire <jamesmburug24@gmail.com>
2022-12-06 05:33:48 +0300

    GWC-8 dependent: :destroy on user for user_cargos

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     validates :username, presence: true, uniqueness: true
 
-    has_many :user_cargos
+    has_many :user_cargos, dependent: :destroy
     has_many :cargo_ships, through: :user_cargos
     
 end
