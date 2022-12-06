ee470c5d7487f5fee4f6e4b7e9b94c6fbe695105
James Mburu Githire <jamesmburug24@gmail.com>
2022-12-06 10:11:15 +0300

    GWC-8 added validations

--- app/models/user_cargo.rb
+++ app/models/user_cargo.rb
@@ -1,5 +1,6 @@
 class UserCargo < ApplicationRecord
-    validates :cargo_ship_id
+    validates :cargo_ship_id, presence: true
+    validates :user_id, presence: true
     belongs_to :user
-    belongs_to :cargo_ships
+    belongs_to :cargo_ship
 end
