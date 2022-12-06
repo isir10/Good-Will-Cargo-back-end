c988ccbd2989ee706d751ead6fb3b63f8b0152c8
James Mburu Githire <jamesmburug24@gmail.com>
2022-12-06 05:44:20 +0300

    GWC-8 added create to user_cargo_controller

--- app/controllers/user_cargos_controller.rb
+++ app/controllers/user_cargos_controller.rb
@@ -7,6 +7,11 @@ class UserCargosController < ApplicationController
         render json: {cargo: userCargo, cargo_ships: cargo_ships}
     end
 
+    def create
+        user_cargo = UserCargo.create!(user_id: session[:user_id], cargo_ship_id: params[:cargo_ship_id], count: params[:count], params[:amount])
+        render json: {cargo: user_cargo}, status: :created
+    end
+
     def update
         cargo_ship = UserCargo.find(params[:id])
         cargo_ship.update!(cargo_ship_params)
