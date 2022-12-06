6c550a9e81525e6192521ca19dfe969a80703037
James Mburu Githire <jamesmburug24@gmail.com>
2022-12-06 12:52:33 +0300

    GWC-8 added destroy action in user_cargo

--- app/controllers/user_cargos_controller.rb
+++ app/controllers/user_cargos_controller.rb
@@ -1,4 +1,5 @@
 class UserCargosController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_method
     before_action :authorize
     def index
         user  = User.find(session[:user_id]) 
@@ -13,13 +14,21 @@ class UserCargosController < ApplicationController
     end
 
     def update
-        cargo_ship = UserCargo.find(params[:id])
+        cargo_ship = UserCargo.find_by!(id: params[:id], user_id: session[:user_id])
         cargo_ship.update!(cargo_ship_params)
         render json: cargo_ship, status: :accepted
     end
-    
+
+    def destroy
+        cargo_ship = UserCargo.find_by!(id: params[:id], user_id: params[:user_id])
+        cargo_ship.destroy
+        head :no_content
+    end
     private
     def authorize
         return render json: {errors: ["Not authorized"]}, status: :unauthorized unless session.include? :user_id
+    end
+    def record_not_found_method
+        render json: { error: "Cargo Not Found" }, status: :not_found
     end
 end
