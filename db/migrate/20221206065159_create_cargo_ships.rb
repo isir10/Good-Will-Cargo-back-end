eba3d6d7048728b5e75772240aaa0cc00d51aefc
+ <+>
2022-12-06 11:43:09 +0300

    added columns to tables

--- db/migrate/20221206065159_create_cargo_ships.rb
+++ db/migrate/20221206065159_create_cargo_ships.rb
@@ -1,6 +1,12 @@
 class CreateCargoShips < ActiveRecord::Migration[7.0]
   def change
     create_table :cargo_ships do |t|
+      t.string :ship_name
+      t.string :img-url
+      t.string :route 
+      t.date :leaving_date
+      
+
 
       t.timestamps
     end
