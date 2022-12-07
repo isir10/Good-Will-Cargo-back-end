d9d40464ed546cfdc9ccff3b2b7c40ff5ad1a258
James Mburu Githire <jamesmburug24@gmail.com>
2022-12-07 20:41:42 +0300

    GWC-8 updated user_cargos_ctrl to use jwt

--- app/controllers/user_cargos_controller.rb
+++ app/controllers/user_cargos_controller.rb
@@ -2,25 +2,28 @@ class UserCargosController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_method
     before_action :authorized
     def index
-        user  = User.find(session[:user_id]) 
+        user_id = decoded_token[0]['user_id']
+        user = User.find(user_id)
         userCargo = user.user_cargos
-        cargo_ships = user.cargo_ships
         render json: userCargo
     end
 
     def create
-        user_cargo = UserCargo.create!(user_id: session[:user_id], cargo_ship_id: params[:cargo_ship_id], count: params[:count], amount: params[:amount])
+        user_id = decoded_token[0]['user_id']
+        user_cargo = UserCargo.create!(user_id: user_id, cargo_ship_id: params[:cargo_ship_id], count: params[:count], amount: params[:amount])
         render json: user_cargo, status: :created
     end
 
     def update
-        cargo_ship = UserCargo.find_by!(id: params[:id], user_id: session[:user_id])
+        user_id = decoded_token[0]['user_id']
+        cargo_ship = UserCargo.find_by!(id: params[:id], user_id: user_id)
         cargo_ship.update!(cargo_ship_params)
         render json: cargo_ship, status: :accepted
     end
 
     def destroy
-        cargo_ship = UserCargo.find_by!(id: params[:id], user_id: session[:user_id])
+        user_id = decoded_token[0]['user_id']
+        cargo_ship = UserCargo.find_by!(id: params[:id], user_id: user_id)
         cargo_ship.destroy
         head :no_content
     end
