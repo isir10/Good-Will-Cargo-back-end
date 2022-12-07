c9518177a627058159417aa51c44d01ba638f27a
James Mburu Githire <jamesmburug24@gmail.com>
2022-12-07 20:42:18 +0300

    GWC-8 updated users_ctrl to use jwt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,32 @@
 class UsersController < ApplicationController
-    before_action :authorize, only: [:show, :update]
+    before_action :authorized, only: [:show, :update]
     def index
         render json: User.all
     end
     def create
         user = User.create!(user_params)
-        session[:user_id] = user.id
-        render json: user, status: :created
+        if user.valid?
+            token = encode_token(user_id: user.id)
+            render json: {user: user, jwt: token}, status: :created
+        else
+            render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
+        end
     end
 
     def show
-        user = User.find(session[:user_id])
+        user_id = decoded_token[0]['user_id']
+        user = User.find(user_id)
         render json: user
     end
 
     def update
-        user = User.find(session[:user_id])
+        user_id = decoded_token[0]['user_id']
+        user = User.find(user_id)
         user.update!(user_params)
         render json: user, status: :accepted
     end
     private
     def user_params
         params.permit(:username, :password, :password_confirmation, :bio, :image_url)
-    end
-
-    def authorize
-        return render json: {errors: ["Not authorized"]}, status: :unauthorized unless session.include? :user_id
     end
 end
