b753aa0bc175fa643cba91c1c4d68c050d5ef10d
+ <+>
2022-12-08 11:54:27 +0300

    added error messages to the app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
-end
+    rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity_method
+        private
+        def unprocessable_entity_method e
+            render json: {errors: e.record.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+    
