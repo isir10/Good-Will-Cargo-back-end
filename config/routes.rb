20b519166efdacacb1d9c4f751efdd0d973ab853
+ <+>
2022-12-08 10:05:31 +0300

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :cargo_ships, only:[:index, :show]
-  
+
   resources :user_cargos, only:[:create, :index, :show, :update, :destroy]
+  
   resources :users
   post "/signup", to: "users#create"
   post "/login", to: "sessions#create"
