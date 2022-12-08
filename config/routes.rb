62a3374fb1d0d75be62d6212cb4c7870371e698b
James Mburu Githire <jamesmburug24@gmail.com>
2022-12-08 18:49:36 +0300

    GWC-8 added delete "/me" to users#destroy in route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   post "/login", to: "jwt#create"
   delete "/logout", to: "jwt#destroy"
   get "/me", to: "users#show"
+  delete "/me", to: "users#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
