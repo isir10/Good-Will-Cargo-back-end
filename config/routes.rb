64cbcfdb086e88bf9371dcdd888e00d29eb5da92
+ <+>
2022-12-08 10:06:06 +0300

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   resources :cargo_ships, only:[:index, :show]
 
   resources :user_cargos, only:[:create, :index, :show, :update, :destroy]
-  
+
   resources :users
+  
   post "/signup", to: "users#create"
   post "/login", to: "jwt#create"
   delete "/logout", to: "jwt#destroy"
