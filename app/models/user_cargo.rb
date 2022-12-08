d5fc380e20d104dda5c24e1e35b84bfe856a9875
James Mburu Githire <jamesmburug24@gmail.com>
2022-12-08 22:27:32 +0300

    GWC-8 added count validation on user_cargo

--- app/models/user_cargo.rb
+++ app/models/user_cargo.rb
@@ -1,6 +1,12 @@
 class UserCargo < ApplicationRecord
     validates :cargo_ship_id, presence: true
     validates :user_id, presence: true
+    validate :count_range
     belongs_to :user
     belongs_to :cargo_ship
+    def count_range
+        if(count > 0){
+            errors.add(:count, "must be greater than 0")
+        }
+    end
 end
