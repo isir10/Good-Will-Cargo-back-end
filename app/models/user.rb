bd4db90210de3ddd6bcf854e2db489a0ecc96c78
+ <+>
2022-12-08 12:02:56 +0300

    added relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
-    
+    has_secure_password
+    validates :username, presence: true, uniqueness: true
+
+    has_many :user_cargos, dependent: :destroy
+    has_many :cargo_ships, through: :user_cargos
 end
