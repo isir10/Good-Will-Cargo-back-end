43100c51c6fe628a80a3624f4534a44e9ba4f399
+ <+>
2022-12-08 10:07:31 +0300

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,9 @@ Rails.application.routes.draw do
   post "/signup", to: "users#create"
 
   post "/login", to: "sessions#create"
-  
+
   delete "/logout", to: "sessions#destroy"
+  
   get "/me", to: "users#show"
  
 end
