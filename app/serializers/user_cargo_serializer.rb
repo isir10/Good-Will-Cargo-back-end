5b97e3e6a13435b0a5fdf735d3c08fab12deafc9
James Mburu Githire <jamesmburug24@gmail.com>
2022-12-09 19:46:09 +0300

    GWC-8 added id attribute to userCargo ctrlr

--- app/serializers/user_cargo_serializer.rb
+++ app/serializers/user_cargo_serializer.rb
@@ -1,4 +1,4 @@
 class UserCargoSerializer < ActiveModel::Serializer
-  attributes :count, :amount, :user_id, :cargo_ship
+  attributes :id, :count, :amount, :user_id, :cargo_ship
   belongs_to :cargo_ship
 end
