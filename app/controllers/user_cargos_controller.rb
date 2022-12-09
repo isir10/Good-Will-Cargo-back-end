baea92971440692d30088de928d87c2e37a3a532
James Mburu Githire <jamesmburug24@gmail.com>
2022-12-09 19:53:18 +0300

    GWC-8 updated backend calculation

--- app/controllers/user_cargos_controller.rb
+++ app/controllers/user_cargos_controller.rb
@@ -24,16 +24,18 @@ class UserCargosController < ApplicationController
         user_cargo = UserCargo.find_by!(id: params[:id], user_id: user_id)
         cargo_ship = CargoShip.find(params[:cargo_ship_id])
         cargo_amount = cargo_ship.rate_per_cargo
-        total_amount = cargo_amount.to_i * params[:count].to_i
-        cargo_ship.update(remaining: (cargo_ship.remaining.to_i - params[:count].to_i))
+        total_amount = cargo_amount.to_i * params[:count].to_i 
+        cargo_ship.update(remaining: (cargo_ship.remaining.to_i - (cargo_ship.remaining.to_i - params[:count].to_i)))
         user_cargo.update!(count: params[:count], amount: total_amount)
         render json: user_cargo, status: :accepted
     end
 
     def destroy
         user_id = decoded_token[0]['user_id']
-        cargo_ship = UserCargo.find_by!(id: params[:id], user_id: user_id)
-        cargo_ship.destroy
+        user_cargo = UserCargo.find_by!(id: params[:id], user_id: user_id)
+        cargo_ship user_cargo.cargo_ship
+        cargo_ship.update(remaining: (cargo_ship.remaining.to_i) - user_cargo.count.to_i)
+        user_cargo.destroy
         head :no_content
     end
     private
